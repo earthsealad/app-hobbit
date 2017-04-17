58be0a6f383547b4633a03e58c70425a5729d8bf
hobbit Do <earthsealad@empal.com>
2017-04-17 01:14:12 -0500

    adpat translator

--- app.js
+++ app.js
@@ -20,17 +20,36 @@ app.use(express.static(__dirname + '/public'));
  
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
- 
-io.on('connection', function(socket) {
-    // When user send message as ‘P’ then broadcast to all users
-    socket.on('P', function(msg) {
-        io.emit('P', msg);
-    });
+ 
+// watson developer cloud SDK/language translation model invoke
+var LanguageTranslatorV2 = require('watson-developer-cloud/language-translator/v2');
+var language_translator = new LanguageTranslatorV2({
+// Bluemix will handle username/password automatically by service
 });
+ 
+io.on('connection', function(socket) {
+    socket.on('P', function(msg) {
+        io.emit('P', msg);
+        language_translator.translate({
+            text: msg,
+            source: 'en',
+            target: 'fr'
+        }, function(err, translation) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(translation);
+                io.emit('P', 'FR: ' + translation.translations[0].translation);
+            }
+        });
+    });
+});
+ 
 // start server on the specified port and binding host
 http.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
 });
